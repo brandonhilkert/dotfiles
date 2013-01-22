559ab4b1765a991f25096583df0e7a4466ec5000
Brandon Hilkert <brandonhilkert@gmail.com>
2013-01-22 09:32:46 -0500

    Update sublime.

--- sublime2/setup.sh
+++ sublime2/setup.sh
@@ -4,10 +4,6 @@ set -x
 
 # symlink settings in
 sublime_dir=~/Library/Application\ Support/Sublime\ Text\ 2/Packages
-mv "$sublime_dir/User" "$sublime_dir/User.backup"
-mkdir "$sublime_dir/User"
-cp -R ~/Dropbox/code/dotfiles/sublime2/User/* "$sublime_dir/User"
-# ln -s "~/Dropbox/code/dotfiles/sublime2/User" "$sublime_dir"
 
 # Grab the Soda theme
 cd "$sublime_dir"
