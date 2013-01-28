189146fd796c538b5acc557b365bd62df6aadebc
Brandon Hilkert <brandonhilkert@gmail.com>
2013-01-28 09:03:17 -0500

    Update links and bashrc.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cp -f ./bash/.* ~/
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/bash/.bashrc /Users/bhilkert/.bashrc
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/bash/.bash_profile /Users/bhilkert/.bash_profile
+
 cp -f ./git/.* ~/
 
 ln -fns /Users/bhilkert/Dropbox/code/dotfiles/vim/.vimrc /Users/bhilkert/.vimrc
