60c6dc231babc0811798f2cd5396ef4609ac1686
Brandon Hilkert <brandonhilkert@gmail.com>
2013-01-28 10:35:51 -0500

    Remove dot notation so files are visible and update links.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/bash/.bashrc /Users/bhilkert/.bashrc
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/bash/.bash_profile /Users/bhilkert/.bash_profile
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/bash/bashrc /Users/bhilkert/.bashrc
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/bash/bash_profile /Users/bhilkert/.bash_profile
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/.git-completion.sh /Users/bhilkert/.git-completion.sh
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/.gitconfig /Users/bhilkert/.gitconfig
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/.gitignore /Users/bhilkert/.gitignore
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/git-completion.sh /Users/bhilkert/.git-completion.sh
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/gitconfig /Users/bhilkert/.gitconfig
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/gitignore /Users/bhilkert/.gitignore
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/vim/.vimrc /Users/bhilkert/.vimrc
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/vim/.vim /Users/bhilkert/.vim
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/vim/vimrc /Users/bhilkert/.vimrc
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/vim/vim /Users/bhilkert/.vim
 
 osx/setup.sh
