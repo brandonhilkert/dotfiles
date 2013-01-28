1256018e996127ab7535898b5cddb22df58b30ae
Brandon Hilkert <brandonhilkert@gmail.com>
2013-01-28 08:47:54 -0500

    Link vim files.

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 
 cp -f ./bash/.* ~/
 cp -f ./git/.* ~/
-cp -f ./vim/.* ~/
+
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/vim/.vimrc /Users/bhilkert/.vimrc
+ln -fns /Users/bhilkert/Dropbox/code/dotfiles/vim/.vim /Users/bhilkert/.vim
+
 osx/setup.sh
