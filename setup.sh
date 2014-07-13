d3306205865e78eda115f371afe2eca6462c5a95
Brandon Hilkert <brandonhilkert@gmail.com>
2014-07-13 13:26:47 -0400

    Update for Google Drive

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/bash/bashrc /Users/bhilkert/.bashrc
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/bash/bash_profile /Users/bhilkert/.bash_profile
+ln -fns /Users/bhilkert/Google\ Drive/code/dotfiles/bash/bashrc /Users/bhilkert/.bashrc
+ln -fns /Users/bhilkert/Google\ Drive/code/dotfiles/bash/bash_profile /Users/bhilkert/.bash_profile
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/git-completion.sh /Users/bhilkert/.git-completion.sh
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/gitconfig /Users/bhilkert/.gitconfig
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/git/gitignore /Users/bhilkert/.gitignore
+ln -fns /Users/bhilkert/Google\ Drive/code/dotfiles/git/git-completion.sh /Users/bhilkert/.git-completion.sh
+ln -fns /Users/bhilkert/Google\ Drive/code/dotfiles/git/gitconfig /Users/bhilkert/.gitconfig
+ln -fns /Users/bhilkert/Google\ Drive/code/dotfiles/git/gitignore /Users/bhilkert/.gitignore
 
 mkdir -p /Users/bhilkert/.vim-tmp/undo
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/vim/vimrc /Users/bhilkert/.vimrc
+ln -fns /Users/bhilkert/Google\ Drive/code/dotfiles/vim/vimrc /Users/bhilkert/.vimrc
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/ack/ackrc /Users/bhilkert/.ackrc
+ln -fns /Users/bhilkert/Google\ Drive/code/dotfiles/ack/ackrc /Users/bhilkert/.ackrc
 
-ln -fns /Users/bhilkert/Dropbox/code/dotfiles/ag/agignore /Users/bhilkert/.agignore
+ln -fns /Users/bhilkert/Google\ Drive/code/dotfiles/ag/agignore /Users/bhilkert/.agignore
 
 osx/setup.sh
