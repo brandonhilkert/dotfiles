8cdfeeac71c7100f461e59587099574553d20ee1
Brandon Hilkert <brandonhilkert@Brandons-2019-MacBook-Pro.local>
2020-04-20 18:22:09 -0700

    Code is no longer in Dropbox

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-ln -fns /Users/brandonhilkert/Dropbox/code/dotfiles/bash/bashrc /Users/brandonhilkert/.bashrc
-ln -fns /Users/brandonhilkert/Dropbox/code/dotfiles/bash/bash_profile /Users/brandonhilkert/.bash_profile
+ln -fns /Users/brandonhilkert/code/dotfiles/bash/bashrc /Users/brandonhilkert/.bashrc
+ln -fns /Users/brandonhilkert/code/dotfiles/bash/bash_profile /Users/brandonhilkert/.bash_profile
 
-ln -fns /Users/brandonhilkert/Dropbox/code/dotfiles/git/git-completion.sh /Users/brandonhilkert/.git-completion.sh
-ln -fns /Users/brandonhilkert/Dropbox/code/dotfiles/git/gitconfig /Users/brandonhilkert/.gitconfig
-ln -fns /Users/brandonhilkert/Dropbox/code/dotfiles/git/gitignore /Users/brandonhilkert/.gitignore
+ln -fns /Users/brandonhilkert/code/dotfiles/git/git-completion.sh /Users/brandonhilkert/.git-completion.sh
+ln -fns /Users/brandonhilkert/code/dotfiles/git/gitconfig /Users/brandonhilkert/.gitconfig
+ln -fns /Users/brandonhilkert/code/dotfiles/git/gitignore /Users/brandonhilkert/.gitignore
 
 mkdir -p /Users/brandonhilkert/.vim-tmp/undo
-ln -fns /Users/brandonhilkert/Dropbox/code/dotfiles/vim/vimrc /Users/brandonhilkert/.vimrc
-ln -fns /Users/brandonhilkert/Dropbox/code/dotfiles/vim/vim /Users/brandonhilkert/.vim
+ln -fns /Users/brandonhilkert/code/dotfiles/vim/vimrc /Users/brandonhilkert/.vimrc
+ln -fns /Users/brandonhilkert/code/dotfiles/vim/vim /Users/brandonhilkert/.vim
 
-ln -fns /Users/brandonhilkert/Dropbox/code/dotfiles/ack/ackrc /Users/brandonhilkert/.ackrc
+ln -fns /Users/brandonhilkert/code/dotfiles/ack/ackrc /Users/brandonhilkert/.ackrc
 
-ln -fns /Users/brandonhilkert/Dropbox/code/dotfiles/ag/agignore /Users/brandonhilkert/.agignore
+ln -fns /Users/brandonhilkert/code/dotfiles/ag/agignore /Users/brandonhilkert/.agignore
 
 osx/setup.sh
